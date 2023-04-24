4700fb206f02e5ba4f09367e314bf189da69b613
Thanasorn <th.thanasorn.lim@gmail.com>
2023-04-24 12:37:41 +0700

    feat: try add serverless function count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,18 +9,23 @@ export const getStaticProps: GetStaticProps = async (
 ) => {
   const name =
     (await (await fetch(`${process.env.BASE_API}hello`))?.json()) || "";
+  const date =
+    (await (await fetch(`${process.env.BASE_API}date`))?.json()) || "";
+  console.log(date);
   return {
     props: {
       name: name.name,
+      date: date.date,
     },
   };
 };
 
 interface HomeProps {
   name: string;
+  date: string;
 }
 
-export default function Home({ name }: HomeProps) {
+export default function Home({ name, date }: HomeProps) {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -50,7 +55,18 @@ export default function Home({ name }: HomeProps) {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <div
+        className={`relative flex place-items-center 
+      before:absolute before:h-[300px] before:w-[480px] 
+      before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial 
+      before:from-white before:to-transparent before:blur-2xl before:content-[''] 
+      after:absolute after:-z-20 after:h-[180px] after:w-[240px] 
+      after:translate-x-1/3 after:bg-gradient-conic 
+      after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] 
+      before:dark:bg-gradient-to-br before:dark:from-transparent 
+      before:dark:to-blue-700/10 after:dark:from-sky-900 
+      after:dark:via-[#0141ff]/40 before:lg:h-[360px]`}
+      >
         {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -59,9 +75,16 @@ export default function Home({ name }: HomeProps) {
           height={37}
           priority
         /> */}
-        <div className="text-8xl font-bold dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert">
+        <div className="text-8xl text-stone-800 font-bold dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert">
           Hello I am {name}
         </div>
+      </div>
+
+      <div>
+        Server Started on:{" "}
+        {Intl.DateTimeFormat("en-GB", {
+          dateStyle: "long",
+        }).format(new Date(date))}
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
